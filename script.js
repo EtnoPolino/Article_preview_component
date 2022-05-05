b51da977ff2dd7b184ec38af312871f61e6ab2b9
EtnoPolino <einshoch66@gmail.com>
2022-05-05 20:54:04 +0200

    add transition and readme update

--- script.js
+++ script.js
@@ -5,4 +5,6 @@ button.addEventListener("click", showSocial);
 
 function showSocial() {
   social.classList.toggle("hide");
+  social.classList.toggle('show-in');
+  social.classList.toggle('slide-in');
 }
